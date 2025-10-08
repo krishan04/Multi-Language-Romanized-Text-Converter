f8cde5007ec677a6bce6d62315a2ade05c9f4cd1
krishan04 <krishanlakhotia04@gmail.com>
2025-10-09 04:08:47 +0530

    Complete

--- backend/src/main/java/com/TextConverter/Multi_Language_Romanized_Text_Converter/controller/TranslationController.java
+++ backend/src/main/java/com/TextConverter/Multi_Language_Romanized_Text_Converter/controller/TranslationController.java
@@ -7,7 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {
+        "https://multi-language-romanized-text-converter-3lck.onrender.com",
+        "http://localhost:5173"
+})
 @RestController
 @RequestMapping("/api")
 public class TranslationController {
